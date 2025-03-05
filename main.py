262a803123d2e60934653e4e42e743bd8d5d7abb
Edouard <spamghettie@gmail.com>
2025-03-05 15:27:07 -0500

    DeepQ class ajoutée avec tous les imports

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import numpy as np
 import torch as T
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
+import random
+
+#
+class DeepQNetwork(nn.Module):
+    def __init__(self): # maybe faut mettre d'autres variables... a voir
+        super(DeepQNetwork, self).__init__() #constructs base classs
